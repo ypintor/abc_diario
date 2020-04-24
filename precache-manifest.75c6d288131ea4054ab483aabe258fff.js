31cd0c2b07485cf8928ae2cdcebdee6154476930
yoco <yoco@uaz.edu.mx>
2020-04-24 17:49:36 -0500

    202004241749

---
+++ precache-manifest.75c6d288131ea4054ab483aabe258fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61404109be973abce9420b5b68779202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db2c5798931a85e4cda6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "db2c5798931a85e4cda6",
+    "url": "/static/js/2.93aba434.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.93aba434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85b4e0c2a3f3049af55",
+    "url": "/static/js/main.af0823d9.chunk.js"
+  },
+  {
+    "revision": "fbb22c25a3824b91cb35",
+    "url": "/static/js/runtime-main.08ca652d.js"
+  }
+]);
